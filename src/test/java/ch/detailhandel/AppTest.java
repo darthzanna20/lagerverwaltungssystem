0049e0352309e3a47697e35bb2257aae9b7c5fbe
mccabe <kilian.mccabe@bluewin.ch>
2024-06-10 22:42:58 +0200

    initial commit

---
+++ src/test/java/ch/detailhandel/AppTest.java
@@ -0,0 +1,19 @@
+package ch.detailhandel;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() throws NullPointerException
+    {
+        assertTrue( true );
+    }
+}
